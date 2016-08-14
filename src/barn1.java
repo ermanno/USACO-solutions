b309a099d056453eaff37420d7ae725993ca79cf
Ermanno Moser <moser.ermanno@gmail.com>
2016-08-14 03:07:58 +0200

    Completed second assignment in the greedy algorithm cathegory.

--- src/barn1.java
+++ src/barn1.java
@@ -12,7 +12,7 @@ class barn1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("barn1.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("barn1.out")));
-		
+
 		StringTokenizer st = new StringTokenizer(f.readLine());
 		int maxNumBoards = Integer.parseInt(st.nextToken());
 		int numStalls = Integer.parseInt(st.nextToken());
@@ -21,21 +21,20 @@ class barn1 {
 		for (int i = 0; i < numCows; i++) {
 			occupiedStallsIndexes.add(Integer.parseInt(f.readLine()));
 		}
-		
+
 		int numOfBlockedStalls = calculateMinimumNumberOfBlockedStalls(maxNumBoards, numStalls, occupiedStallsIndexes);
-		
+
 		out.println(numOfBlockedStalls);
 		out.close();
 	}
 
-	public static int calculateMinimumNumberOfBlockedStalls(int maxNumBoards,
-															int numStalls,
-															ArrayList<Integer> occupiedStallsIndexes) {
+	public static int calculateMinimumNumberOfBlockedStalls(int maxNumBoards, int numStalls,
+			ArrayList<Integer> occupiedStallsIndexes) {
 
 		// we want the indexes to be ordered
 		Collections.sort(occupiedStallsIndexes);
-		
-		// the idea is to split where we have the largest "empty" interval 
+
+		// the idea is to split where we have the largest "empty" interval
 		// between the starting and ending indexes
 		int numBlockedStalls = 1;
 		int numUsedBoards = 1;
@@ -43,33 +42,36 @@ class barn1 {
 		for (int i = 1; i < occupiedStallsIndexes.size(); i++) {
 			int firstIndex = occupiedStallsIndexes.get(i - 1);
 			int secondIndex = occupiedStallsIndexes.get(i);
-			
-			// the first has already been accounted for (either outside the loop, 
-			// or in the case of the current second, we account for it now, 
-			// but in the next iteration it will be the next "first" that we will 
+
+			// the first has already been accounted for (either outside the
+			// loop,
+			// or in the case of the current second, we account for it now,
+			// but in the next iteration it will be the next "first" that we
+			// will
 			// have already accounted for.
 			numBlockedStalls += 1;
-			
+
 			int space = secondIndex - 1 - firstIndex;
 			if (space > 0) {
 				spaces.add(space);
 			}
 		}
-		
+
 		// We order the spaces by size
 		Collections.sort(spaces, Collections.reverseOrder());
-				
+
 		// we remove the stalls that are part of the "empty" section
 		while (numUsedBoards < maxNumBoards && !spaces.isEmpty()) {
 			numUsedBoards += 1;
 			spaces.remove(0);
 		}
-		
-		// we add to the total number of blocked stalls the rest of the empty stalls
+
+		// we add to the total number of blocked stalls the rest of the empty
+		// stalls
 		for (Integer space : spaces) {
 			numBlockedStalls += space;
 		}
-		
+
 		return numBlockedStalls;
 	}
 }
