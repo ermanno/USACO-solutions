1f8c82de0a36f565d619275c362ac72feb47180b
Ermanno Moser <moser.ermanno@gmail.com>
2016-08-14 03:09:58 +0200

    Completed second assignment in the greedy algorithm cathegory.

--- src/barn1.java
+++ src/barn1.java
@@ -31,11 +31,11 @@ class barn1 {
 	public static int calculateMinimumNumberOfBlockedStalls(
 			int maxNumBoards, int numStalls, ArrayList<Integer> occupiedStallsIndexes) {
 
-		// we want the indexes to be ordered
+		// We want the indexes to be ordered.
 		Collections.sort(occupiedStallsIndexes);
 
-		// the idea is to split where we have the largest "empty" interval
-		// between the starting and ending indexes
+		// The idea is to split where we have the largest "empty" interval
+		// between the starting and ending indexes.
 		int numBlockedStalls = 1;
 		int numUsedBoards = 1;
 		ArrayList<Integer> spaces = new ArrayList<Integer>();
@@ -43,12 +43,10 @@ class barn1 {
 			int firstIndex = occupiedStallsIndexes.get(i - 1);
 			int secondIndex = occupiedStallsIndexes.get(i);
 
-			// the first has already been accounted for (either outside the
-			// loop,
+			// The first has already been accounted for (either outside the loop
 			// or in the case of the current second, we account for it now,
 			// but in the next iteration it will be the next "first" that we
-			// will
-			// have already accounted for.
+			// will have already accounted for.
 			numBlockedStalls += 1;
 
 			int space = secondIndex - 1 - firstIndex;
@@ -57,17 +55,16 @@ class barn1 {
 			}
 		}
 
-		// We order the spaces by size
+		// We order the spaces by size.
 		Collections.sort(spaces, Collections.reverseOrder());
 
-		// we remove the stalls that are part of the "empty" section
+		// We remove the stalls that are part of the "empty" section
 		while (numUsedBoards < maxNumBoards && !spaces.isEmpty()) {
 			numUsedBoards += 1;
 			spaces.remove(0);
 		}
 
-		// we add to the total number of blocked stalls the rest of the empty
-		// stalls
+		// We add to the total number of blocked stalls the rest of the empty stalls.
 		for (Integer space : spaces) {
 			numBlockedStalls += space;
 		}
